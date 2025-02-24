6414777ba551ca5f92931ec6a7dc7645c1dd1381
T3Nzw <t3nszw@gmail.com>
2025-02-25 00:22:34 +0200

    Fix mistake and optimise algorithm

--- week_05/solutions/ex6.cpp
+++ week_05/solutions/ex6.cpp
@@ -2,13 +2,16 @@
 
 void bubbleSort(int arr[], int size) {
   for (int i = 0; i < size-1; ++i) {
-    for (int j = i+1; j < size; ++j) {
-      if (arr[i] > arr[j]) {
+    bool swapped = false;
+    for (int j = 0; j < size-1-i; ++j) {
+      if (arr[j] > arr[j+1]) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+        swapped = true;
       }
     }
+    if (!swapped) return;
   }
 }
 
